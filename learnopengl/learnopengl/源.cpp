53b0517b0fef215b450eb6960627a92d0f378bba
Yiki <2284174996@qq.com>
2019-11-13 17:46:52 +0800

    建立opengl练习项目
    建立opengl练习项目

---
+++ learnopengl/learnopengl/源.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<vector>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<assimp/camera.h>
+#include<assimp/Importer.hpp>
+#include<assimp/scene.h>
+#include<assimp/postprocess.h>
+
+
+using namespace std;
+
+int main()
+{			  
+	return 0;
+}
